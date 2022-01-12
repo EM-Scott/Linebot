eb08f92ebe7b4aade8321911583f14563df51df3
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-01-12 23:43:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ line_bot_api = LineBotApi('lPkuq0new8upb+bh5muA9vU9w/BNy5+QQhk7r3cFxqdL9wcv6n2ue
 # Channel Secret
 handler = WebhookHandler('e8a1992d6f0fa55a5509d6f7145835b0')
 
+line_bot_api.push_message('U13827e14d459bb54ca2e0357703e920e', TextSendMessage(text='你可以開始了'))
+
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
