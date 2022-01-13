7a9a41915084d4a5df9f261380c2ed99c38a278b
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-01-13 16:53:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,151 +1,175 @@
+from datetime import datetime
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    StickerMessage, StickerSendMessage,
+    ConfirmTemplate, TemplateSendMessage,
+    MessageAction, URIAction, LocationMessage,
+    ButtonsTemplate,
+    FollowEvent
 )
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
 
-import re
-
-
-#======這裡是呼叫的檔案內容=====
-from message import *
-from new import *
-from Function import *
-#======這裡是呼叫的檔案內容=====
-
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import time
-#======python的函數庫==========
-
-app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
 line_bot_api = LineBotApi('lPkuq0new8upb+bh5muA9vU9w/BNy5+QQhk7r3cFxqdL9wcv6n2ue1/jxzWPiCBXSvo0agpYhE4X55liDKoAz6yxoOFxwL/FCUtjEX3TQz+IFDzwuWNmYFxpSgaVenl3Qn4lwPVM7n7FL79qK5DagAdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
 handler = WebhookHandler('e8a1992d6f0fa55a5509d6f7145835b0')
 
 line_bot_api.push_message('U13827e14d459bb54ca2e0357703e920e', TextSendMessage(text='你可以開始了'))
 
+users = {}
 
-# 監聽所有來自 /callback 的 Post Request
+def check_user(id, name):
+    global users
+
+    if id not in users:
+        users[id] = {
+            'name':name,
+            'logs':{'日期時間':'', '經緯度':'', '地址':'', '事由':''},
+            'save':False 
+        }
+
+app = Flask(__name__)
+
+def reply_text(token, id, txt):
+    global users
+    me = users[id]
+
+    if me['save']  == False:
+        if '報修' in txt:
+            queries = ConfirmTemplate(
+                text=f"{me['name']}您好，請問要回報查修地點嗎？", 
+                actions=[
+                    # MessageAction(label='是的', text='回報地點'),
+                    URIAction(
+                        label='回報地點',
+                        uri='line://nv/location'
+                    ),
+                    MessageAction(label='不需要', text='不需要')
+                ])
+            # queries = ButtonsTemplate(
+            #     text=f"{me['name']}您好，請問要回報查修地點嗎？",
+            #     actions=[
+            #         URIAction(
+            #             label='回報地點',
+            #             uri='line://nv/location'
+            #         ),
+            #         MessageAction(label='不需要', text='不需要'),
+            #         URIAction(
+            #             label='前往swf.com.tw網站',
+            #             uri='https://swf.com.tw/'
+            #         )
+            #     ])
+
+            temp_msg = TemplateSendMessage(alt_text='確認訊息',
+                                        template=queries)
+            line_bot_api.reply_message(token, temp_msg)
+            me['save'] = True # 開始紀錄訊息
+        else:
+            line_bot_api.reply_message(
+                token,
+                TextSendMessage(text="收到訊息了，謝謝！"))
+    else:
+        if txt=='不需要':
+            line_bot_api.reply_message(
+                token,
+                TextSendMessage(text="好的，請大致描述狀況。"))
+        elif me['logs']['事由'] == '':
+            line_bot_api.reply_message(
+                token,
+                TextSendMessage(text="我記下來了，辛苦您了！"))
+            me['logs']['事由'] = txt  # 儲存事由
+            # 日期要設置成台北時間
+            dt = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+            me['logs']['日期時間'] = dt
+            me['save'] = False   # 紀錄完畢
+
+            print('資料紀錄:', me['logs'])
+            # logs = [id, me['name'], me['logs']['日期時間'], 
+            #             me['logs']['經緯度'], me['logs']['地址'], me['logs']['事由']]
+
+@app.route('/')
+def index():
+    return 'Welcome to Line Bot!'
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
 
+@handler.default()
+def default(event):
+    print('捕捉到事件：', event)
 
-# 處理訊息
+# 處理「被加入好友」訊息
+@handler.add(FollowEvent)
+def followed(event):
+    _id = event.source.user_id
+    profile = line_bot_api.get_profile(_id)
+    # 紀錄用戶資料
+    _name = profile.display_name
+    print('歡迎新好友，ID：', _id)
+    print('名字：', _name)
+
+# 處理文字訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    if '最新合作廠商' in msg:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '最新活動訊息' in msg:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in msg:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in msg:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
+    _id = event.source.user_id
+    profile = line_bot_api.get_profile(_id)
+    # 紀錄用戶資料
+    _name = profile.display_name
+    print('大頭貼網址：', profile.picture_url)
+    print('狀態消息：', profile.status_message)
+    check_user(_id, _name)
+
+    txt=event.message.text
+
+    reply_text(event.reply_token, _id, txt)
+
+# 處理地點訊息
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    global users
+
+    _id = event.source.user_id
+    me = users[_id]
+    addr=event.message.address    # 地址
+    lat=str(event.message.latitude)    # 緯度
+    lon=str(event.message.longitude)   # 經度
+
+    if addr is None:
+        msg=f'收到GPS座標：({lat}, {lon})\n謝謝您！'
     else:
-        message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message)
+        msg=f'收到GPS座標：({lat}, {lon})。\n地址：{addr}\n謝謝您！'
 
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
+    if  me['save']:
+        me['logs']['經緯度'] = f'({lat}, {lon})'
+        me['logs']['地址'] = addr
 
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-    
-   
-        
-import re
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = event.message.text
-    if re.match("台鐵時刻",message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage("https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip112/gobytime"))
-    elif re.match("三點一刻",message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage("https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip112/gobytime"))
-    elif re.match("GG88",message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage("巴拉八八八"))
-    elif re.match("你是誰",message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage("才不告訴逆雷"))
-        
+        line_bot_api.reply_message(
+            event.reply_token, [
+                TextSendMessage(text=msg),
+                TextSendMessage(text='請問是什麼狀況呢？')
+        ])
     else:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
-        
-        
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # get user id when reply
-    user_id = event.source.user_id
-    print("user_id =", user_id)
-    
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=msg))
+
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = event.message.text
-    if re.match("守望傳說",message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage("金價讚啦"))
-        url = 'https://www.guardiantales.com/coupon/redeem/'
-        UsersID = ['825722946229','989122951121']
-        GRCodes = ['XMASCAROL']
-
-        for i in UsersID:
-            for j in GRCodes:
-                class bcolors:
-                    OK= '\033[92m' #GREEN
-                    WARNING= '\033[93m' #YELLOW
-                    FAIL= '\033[91m' #RED
-                    RESET= '\033[0m' #RESET COLOR
-                    
-                payload=('region=SEA&' + 'userId=' + i +'&code=' + j )
-                headers = {
-                    'content-type': "application/x-www-form-urlencoded",
-                }
-                response = requests.request("POST", url, data=payload, headers=headers) # 送出資訊
-                soup = BeautifulSoup(response.text,"html.parser")
-                sel = soup.find('p').text #抓回顯示值
-                line_bot_api.reply_message(event.reply_token,TextSendMessage(sel))
-
-                
+        StickerSendMessage(
+            package_id=event.message.package_id,
+            sticker_id=event.message.sticker_id)
+    )
 
 import os
 if __name__ == "__main__":
