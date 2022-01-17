cd2c236a0e05ad8dd7378fa5a98968379c4f8a10
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-01-17 23:16:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ handler = WebhookHandler('e8a1992d6f0fa55a5509d6f7145835b0')
 line_bot_api.push_message('U13827e14d459bb54ca2e0357703e920e', TextSendMessage(text='機器人運行開始'))
 my_user_id = 'U13827e14d459bb54ca2e0357703e920e'
 line_bot_api.push_message(my_user_id, TextSendMessage(text='start'))
+line_bot_api.push_message(U13827e14d459bb54ca2e0357703e920e, TextSendMessage(text='機器人重啟完成'))
 
 words = ''
 save = False
