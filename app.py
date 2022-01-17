bd4c39d57693a43898b6478297ade0fd30102a01
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-01-17 21:48:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -107,8 +107,9 @@ def handle_message(event):
     elif '功能列表' in txt:
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
-    else 'Youtube:' in txt:
+    elif 'Youtube:' in txt:
         reply = '此功能開發中'
+   
     msg = TextSendMessage(reply)
     line_bot_api.reply_message(event.reply_token, msg)
     
