7d1dfd8927ec981891839a6340449454385137d1
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-01-21 21:57:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -162,5 +162,5 @@ def handle_sticker_message(event):
     )
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
-    # app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
