c2eb5c6184c0af09c06136c9f9b56360d974511b
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-01-21 16:42:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from collections import UserDict
 from flask import Flask, request, abort
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
 import re
-
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as sac
+from model import sheet
 
 #======這裡是呼叫的檔案內容=====
 from message import *
@@ -30,28 +28,87 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 line_bot_api = LineBotApi('lPkuq0new8upb+bh5muA9vU9w/BNy5+QQhk7r3cFxqdL9wcv6n2ue1/jxzWPiCBXSvo0agpYhE4X55liDKoAz6yxoOFxwL/FCUtjEX3TQz+IFDzwuWNmYFxpSgaVenl3Qn4lwPVM7n7FL79qK5DagAdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('e8a1992d6f0fa55a5509d6f7145835b0')
+#
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+# 建立憑證
+cr = sac.from_json_keyfile_name('D:\Python\Learn\Python 教材\F9796\ch07\model\google_auth.json', scope)  # 請自行修改檔名
+gs = gspread.authorize(cr)
 my_user_id = 'U13827e14d459bb54ca2e0357703e920e'
 line_bot_api.push_message(my_user_id, TextSendMessage(text='機器人運行開始'))
 
-
-
-words = ''
-save = False
 users = {}
 
-def check_user(_id, name):
+def check_user(id, name):
     global users
 
-    if _id not in users and users[_id] is None:
-        users[_id] = {    # 初始化此使用者物件
+    if id not in users and users[id] is None:
+        users[id] = {    # 初始化此使用者物件
             'name':name,
             'words':'',
             'save':False 
         }
-        print('新增一名用戶：', _id)
+        print('新增一名用戶：', id)
     else:
-        print('用戶已經存在，id：', _id)
+        print('用戶已經存在，id：', id)
         print('目前用戶數：', len(users))
+
+def reply_text(token, id, txt):
+    global users
+    me = users[id]
+
+    if me['save']  == False:
+        if '報修' in txt:
+            queries = ConfirmTemplate(
+                text=f"{me['name']}您好，請問要回報查修地點嗎？", 
+                actions=[
+                    URIAction(
+                        label='回報地點',
+                        uri='line://nv/location'
+                    ),
+                    MessageAction(label='不需要', text='不需要')
+                ])
+            # queries = ButtonsTemplate(
+            #     text=f"{me['name']}您好，請問要回報查修地點嗎？",
+            #     actions=[
+            #         URIAction(
+            #             label='回報地點',
+            #             uri='line://nv/location'
+            #         ),
+            #         MessageAction(label='不需要', text='不需要'),
+            #         URIAction(
+            #             label='前往swf.com.tw網站',
+            #             uri='https://swf.com.tw/'
+            #         )
+            #     ])
+
+            temp_msg = TemplateSendMessage(alt_text='確認訊息',
+                                        template=queries)
+            line_bot_api.reply_message(token, temp_msg)
+            me['save'] = True # 開始紀錄訊息
+        else:
+            line_bot_api.reply_message(
+                token,
+                TextSendMessage(text="收到訊息了，謝謝！"))
+    else:
+        if txt=='不需要':
+            line_bot_api.reply_message(
+                token,
+                TextSendMessage(text="好的，請大致描述狀況。"))
+        elif me['logs']['事由'] == '':
+            line_bot_api.reply_message(
+                token,
+                TextSendMessage(text="我記下來了，辛苦您了！"))
+            me['logs']['事由'] = txt  # 儲存事由
+            # 日期要設置成台北時間
+            dt = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+            me['logs']['日期時間'] = dt
+            me['save'] = False   # 紀錄完畢
+
+            print('資料紀錄:', me['logs'])
+            logs = [id, me['name'], me['logs']['日期時間'], 
+                        me['logs']['經緯度'], me['logs']['地址'], me['logs']['事由']]
+            gs.append_row(logs)
 
 
 # 監聽所有來自 /callback 的 Post Request
@@ -72,10 +129,14 @@ def callback():
 #======回應內容======
 
 # 處理訊息
+@handler.default()
+def default(event):
+
 @handler.add(PostbackEvent)
 def handle_message(event):
     print(event.postback.data)    
-    
+
+# 處理文字訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     _id = event.source.user_id
@@ -88,32 +149,30 @@ def handle_message(event):
     print("使用者ID：", profile.user_id)
     check_user(_id, _name)
 
-    
-
-    if '牛牛' in txt:
-        reply = '牛牛啊咪波'
-    elif '教學' in txt:
-        reply = '給您參考:https://github.com/ChenTsungYu/stock_linebot_public/blob/master/app.py'
-    elif '最新合作廠商' in txt:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '最新活動訊息' in txt:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in txt:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in txt:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in txt:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in txt:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif 'Youtube:' in txt:
-        reply = '此功能開發中'
+    # if '牛牛' in txt:
+    #     reply = '牛牛啊咪波'
+    # elif '教學' in txt:
+    #     reply = '給您參考:https://github.com/ChenTsungYu/stock_linebot_public/blob/master/app.py'
+    # elif '最新合作廠商' in txt:
+    #     message = imagemap_message()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '最新活動訊息' in txt:
+    #     message = buttons_message()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '註冊會員' in txt:
+    #     message = Confirm_Template()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '旋轉木馬' in txt:
+    #     message = Carousel_Template()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '圖片畫廊' in txt:
+    #     message = test()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '功能列表' in txt:
+    #     message = function_list()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif 'Youtube:' in txt:
+    #     reply = '此功能開發中'
 
 
    
@@ -121,6 +180,11 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, msg)
     reply_text(event.reply_token, _id, txt)
     
+
+# 處理地點訊息
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+
 
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
