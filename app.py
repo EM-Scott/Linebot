b58f297663b8b86bc6c948aef232c9f7a6feaefc
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-02-04 15:24:26 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+### it can run by 2022/02/04 for call issue. ###
+from model import sheet
+from datetime import datetime
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    StickerMessage, StickerSendMessage,
+    ConfirmTemplate, TemplateSendMessage,
+    MessageAction, URIAction, LocationMessage,
+    ButtonsTemplate
+)
+import os
+
+gs = sheet.GoogleSheet('LineBotDATA','工作表1')
+
+line_bot_api = LineBotApi('lPkuq0new8upb+bh5muA9vU9w/BNy5+QQhk7r3cFxqdL9wcv6n2ue1/jxzWPiCBXSvo0agpYhE4X55liDKoAz6yxoOFxwL/FCUtjEX3TQz+IFDzwuWNmYFxpSgaVenl3Qn4lwPVM7n7FL79qK5DagAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('e8a1992d6f0fa55a5509d6f7145835b0')
+
+
+
+@app.route('/')
+def index():
+    return 'Welcome to Line Bot!'
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.default()
+def default(event):
+    print('捕捉到事件：', event)
+
+# 處理文字訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = str(event.message.text).upper().strip() # 使用者輸入的內容
+    profile = line_bot_api.get_profile(event.source.user_id)
+    user_name = profile.display_name #使用者名稱
+    uid = profile.user_id # 發訊者ID
+#================================ 
+    # 問卷
+    if re.match("你好", msg):
+        line_bot_api.push_message(uid, TextSendMessage(text:msg))
+        return 0
+#處理貼圖訊息
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(
+            package_id=event.message.package_id,
+            sticker_id=event.message.sticker_id)
+    )
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
