dd7ae141eaa8f8b190954742acd1a1e7d75a9518
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-02-04 17:05:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ gs = sheet.GoogleSheet('LineBotDATA','工作表1')
 line_bot_api = LineBotApi('lPkuq0new8upb+bh5muA9vU9w/BNy5+QQhk7r3cFxqdL9wcv6n2ue1/jxzWPiCBXSvo0agpYhE4X55liDKoAz6yxoOFxwL/FCUtjEX3TQz+IFDzwuWNmYFxpSgaVenl3Qn4lwPVM7n7FL79qK5DagAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('e8a1992d6f0fa55a5509d6f7145835b0')
 
+UIDD = ''
+SNN = ''
+save = False
+
 
 app = Flask(__name__)
 
@@ -51,9 +55,8 @@ def handle_message(event):
     uid = profile.user_id # 發訊者ID
 #================================ 
     # AnswerFile
-    UIDD = ''
-    SNN = ''
-    save = False
+    global UIDD
+    global save 
     if re.match("守望兌換", msg):
         if UIDD !='':
             line_bot_api.push_message(uid, TextSendMessage(text={UIDD}))
