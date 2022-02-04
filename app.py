328af24d00dfed6fb88a893a7576958ae471b2fe
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-02-04 16:48:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def handle_message(event):
 
     if re.match("守望兌換", msg):
         if UIDD !='':
-            line_bot_api.push_message(uid, TextSendMessage(text=f'你的UID是:\n\n{UIDD})
+            line_bot_api.push_message(uid, TextSendMessage(text=f'你的UID是:\n\n{UIDD}))
         elif:
             line_bot_api.push_message(uid, TextSendMessage(text='請輸入您的UID:')
             save = True
