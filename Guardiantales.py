70b69118b87ec37534d3d1980ec89fed19bb48e5
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-02-04 14:27:31 +0800

    Update Guardiantales.py

--- Guardiantales.py
+++ Guardiantales.py
@@ -1,43 +1,34 @@
-mport requests
+import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.guardiantales.com/coupon/reddem/"
+url = 'https://www.guardiantales.com/coupon/redeem/'
 
-OK = '\033[92m' #GREEN
-            WARNING = '\033[93m' #YELLOW
-            FAIL = '\033[91m' #RED
-            RESET = '\033[0m' #reset color
+UsersID = ['825722946229','733122961641','477722973249','615722923499','989122951121','885827936452']
+GRCodes = ['HAPPYNEWYEAR','FRESHSTART','WINTERBLUESWHO','WORLDCOLDCOFFEEWARM']
 
-def UserID(UID):
-    
-UserID = ['124564563','212312323', '']
-GRCode = ['defart', 'qwert']
-for i in UserID:
-    for j in GRCode:
+for i in UsersID:
+    for j in GRCodes:
         class bcolors:
-            OK = '\033[92m' #GREEN
-            WARNING = '\033[93m' #YELLOW
-            FAIL = '\033[91m' #RED
-            RESET = '\033[0m' #reset color
-
-        payload=('region=SEA&' + 'userID=' + i + '&code=' + j )
+            OK= '\033[92m' #GREEN
+            WARNING= '\033[93m' #YELLOW
+            FAIL= '\033[91m' #RED
+            RESET= '\033[0m' #RESET COLOR
+            
+        payload=('region=SEA&' + 'userId=' + i +'&code=' + j )
         headers = {
-            'contect-type': "application/x-www-form-urlencoded",
+            'content-type': "application/x-www-form-urlencoded",
         }
-        response = requests.request("POST", url, data=payload, headers=headers)
+        response = requests.request("POST", url, data=payload, headers=headers) # 送出資訊
         soup = BeautifulSoup(response.text,"html.parser")
-        sel = soup.find('p')
-        print(payload)
-        #print(soup)
-        print(sel)
-
-        if sel == "We were unable to find your User Number.Please double-check your User Number and the Region selected and try again.":
-            print(成功)
-        elif sel == "":
-            print(失敗一)
-        elif sel == "":
-            print(失敗二）
-        elif sel == "":
-            print(失敗三）
-        elif sel == "":
-            print(失敗四）
+        sel = soup.find('p').text #抓回顯示值
+        #print(sel)
+        if sel == "Congratulations!You've successfully claimed the Coupon!Please check your mail in-game in order to redeem your rewards.":
+            print(bcolors.OK + '成功。 ' + bcolors.RESET + i + "：" + '兌換' + j)
+        elif sel == 'We were unable to find your User Number.Please double-check your User Number and the Region selected and try again.' :
+            print(bcolors.FAIL + ' 失敗。 ' + bcolors.RESET + '原因：UserID Error' +' (' + i + '_兌換_' + j + ')')
+        elif sel == "The Coupon Code you've entered is invalid.Please check the Coupon Code and try again.":
+            print(bcolors.FAIL + ' 失敗。 ' + bcolors.RESET + '原因：序號錯誤' +' (' + i + '_兌換_' + j + ')')
+        elif sel == "The Coupon Code you've entered has already been claimed.If you have not yet redeemed this Coupon Code, please double-check your User Number and the Region selected and try again." :
+            print(bcolors.FAIL + ' 失敗。 ' + bcolors.RESET + '原因：序號已兌換' +' (' + i + '_兌換_' + j + ')')
+        elif sel == "The Coupon Code you've entered has already expired." :    
+            print(bcolors.FAIL + ' 失敗。 ' + bcolors.RESET + '原因：序號過期' +' (' + i + '_兌換_' + j + ')')
