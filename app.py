8229b081cc17f1473ffb44d5cd73279fadbf7f6f
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-02-08 16:06:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -93,6 +93,8 @@ def handle_message(event):
             #print(group_f)
             #print(group_u)
             line_bot_api.push_message(uid, TextSendMessage(user_name + '您好'))
+    elif re.match("你過來一下", msg):
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=@Viktoria + '就速擬')
         
             
 #處理貼圖訊息
