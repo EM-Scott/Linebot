b5160ddd701976311b9ae41ca9f4f0c315764f48
EM-Scott <97587963+EM-Scott@users.noreply.github.com>
2022-02-08 10:22:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from linebot.models import (
 )
 import taiwanlottery
 from Guardiantales import guard
+from Guardiantales import check_name
 import os
 import re
 import requests
@@ -78,7 +79,7 @@ def handle_message(event):
                 guard_name.append(check_name(i))
                 for j in GRCodes:
                     guard_name.append(guard(i,j))
-                line_bot_api.push_message(uid, TextSendMessage(guard_name))
+                line_bot_api.push_message(uid, TextSendMessage("\n".join(guard_name) + "\n")
         else:
             line_bot_api.push_message(uid, TextSendMessage(user_name + '無使用權限'))
             print(uid)
